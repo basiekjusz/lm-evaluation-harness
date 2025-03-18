2a641ac0ccd2446ec349cb64ad877550906a5f7f
Jasiek Busz <jasiekbusz@gmail.com>
2025-03-18 23:01:31 +0100

    fix

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -460,7 +460,6 @@ class Task(abc.ABC):
                 metadata=(self.config["task"], doc_id, self.config.repeats),
                 apply_chat_template=apply_chat_template,
                 chat_template=chat_template,
-                apply_reasoning=apply_reasoning,
             )
 
             if not isinstance(inst, list):
